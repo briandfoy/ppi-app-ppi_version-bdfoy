94e316bd938af8db590f01d7a12b6ffa67e11eec
brian d foy <brian.d.foy@gmail.com>
2015-01-03 17:05:43 -0500

    Update copyright statement

--- lib/PPI/App/ppi_version/BDFOY.pm
+++ lib/PPI/App/ppi_version/BDFOY.pm
@@ -331,7 +331,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2008-2013, brian d foy, All Rights Reserved.
+Copyright © 2008-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
